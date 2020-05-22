45fe9f3585a0a37e5b89c97c6d43e92b73e9158f
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-22 17:02:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,11 @@
-class student
- def hello
-   puts "Hey there! I'm so excited to learn stuff."
- end
+class Student
 
- def raise_hand
-   puts "Pick me!"
- end
+  def hello
+    puts "Hey there! I'm so excited to learn stuff."
+  end
+
+  def raise_hand
+    puts "Pick me!"
+  end
 
 end
